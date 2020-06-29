26b74bd7094170e03c8ccf82ee1a289c699d531f
CARTOGIS33 <61816683+CARTOGIS33@users.noreply.github.com>
2020-06-29 14:40:20 +0200

    Add files via upload

---
+++ data/BIEN_IMMO_site_4.js
@@ -0,0 +1,11 @@
+var json_BIEN_IMMO_site_4 = {
+"type": "FeatureCollection",
+"name": "BIEN_IMMO_site_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1.0, "Descriptio": "\nCommune 1_LOUE maison 5 piéces 270.000€", "Photo": "<img src =\"https:\/\/wtf.roflcopter.fr\/pics\/kxEqjsn2\/p92v7xM6.JPG\" width=\"200\" height=\"125\" alt=\"Alias Name\"\/>", "NOM": "A_LOUER" }, "geometry": { "type": "Point", "coordinates": [ -0.652650091662756, 44.94280357917431 ] } },
+{ "type": "Feature", "properties": { "id": 2.0, "Descriptio": "\nCommune 1_vends maison 7 piéces 580.000€", "Photo": "<img src =\"https:\/\/wtf.roflcopter.fr\/pics\/RmS1aOSc\/0Gn3ww08.JPG\" width=\"200\" height=\"125\" alt=\"Alias Name\"\/>", "NOM": "A_VENDRE" }, "geometry": { "type": "Point", "coordinates": [ -0.676054852995904, 44.966226459508299 ] } },
+{ "type": "Feature", "properties": { "id": 3.0, "Descriptio": "\nCommune 2_VENDU APPARTEMENT 4 piéces 195.000€", "Photo": "<img src =\"https:\/\/wtf.roflcopter.fr\/pics\/0EFppboc\/ZapaWmft.JPG\" width=\"200\" height=\"125\" alt=\"Alias Name\"\/>", "NOM": "VENDU" }, "geometry": { "type": "Point", "coordinates": [ -0.350426291028038, 44.918150983742571 ] } },
+{ "type": "Feature", "properties": { "id": 4.0, "Descriptio": "\nCommune 3_vends  CHATEAU 2.900000€", "Photo": "<img src =\"https:\/\/wtf.roflcopter.fr\/pics\/WDLGLyuG\/0XUUklkx.JPG\" width=\"200\" height=\"125\" alt=\"Alias Name\"\/>", "NOM": "A_VENDRE" }, "geometry": { "type": "Point", "coordinates": [ -0.08432126948043, 44.933843085109451 ] } }
+]
+}
